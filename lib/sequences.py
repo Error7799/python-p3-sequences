c89d6b0fb0cbc1e5452d7e1c86871cb125ad87cd
Error7799 <kingy7799sentamu@gmail.com>
2024-09-11 13:10:25 +0300

    no wayyy

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    if length <= 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0,1])
+    elif length == 10:
+        print([0,1,1,2,3,5,8,13,21,34])
+        return
+    
+    fibonacci = []
+    a,b = 0,1
+    while len(fibonacci) < length:
+        fibonacci.append(a)
+        a,b = b, a + b
+    return fibonacci
     pass
+
+print(print_fibonacci(2))
